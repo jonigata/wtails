3a0d746e5c44e8eef83682665c3540da39442b4d
hirayama <hirayama@creatures.co.jp>
2013-01-11 12:40:20 +0900

    erase warning

--- lib/wtails/web_server.rb
+++ lib/wtails/web_server.rb
@@ -17,7 +17,7 @@ module Wtails
         File.exist?(path) && File.read(path)
       end
       set :views, File.expand_path("../../../views/", __FILE__)
-      set :public, File.expand_path("../../../public/", __FILE__)
+      set :public_folder, File.expand_path("../../../public/", __FILE__)
 
       get "/" do
         redirect "/single"
